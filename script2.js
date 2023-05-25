1a41eee154fd7f2e5c7d9884b75e5bae9712821b
your-m1nd <vi.neurogirl@gmail.com>
2023-05-26 01:03:55 +0300

    change script

--- script2.js
+++ script2.js
@@ -1,14 +1,3 @@
-//function getInputValue() {
-    //let typeName = document.getElementById('typeName').value;
-    //let paragraf = document.getElementById('paragraf');
-    //paragraf = typeName.value.toLowerCase();
-        //let str = typeName.value.toLowerCase();
-        //let str2 = str[0].toUpperCase()+ str.slice(1);
-        //paragraf.textContent = str2;
-       // };
-
-
-
 let typeName = document.getElementById('typeName');
 typeName.addEventListener('keydown', function (event) {
 });
@@ -19,24 +8,55 @@ let img = document.getElementById('img');
 typeLink.addEventListener('keyup', function(evt) {
     });
 
+
 let commentEnter = document.getElementById('commentEnter');
 let comment = document.getElementById('comment');
 commentEnter.addEventListener('keyup', function(evt) {
-    })
+
+    function checkSpam() {
+      let comment1 = commentEnter.value.includes('ViAgRa');
+      console.log(comment1);
+    }
+})
+
+
+
+//function checkSpam(str) {
+
+    //let newStr = commentEnter.value;
+
+ //'buy ViAgRA now'.includes('ViAgRa').replace(ViAgRA/i, '***'); // true
+//'free xxxxx'.includes('xxxxx').replace(xxx/i, '***');
+//}
 
 
 let send = document.getElementById('send');
     send.onclick = function getInputValue() {
-    paragraf.textContent = typeName.value.toLowerCase();
+
     let newName = typeName.value.toLowerCase();
     let newName2 = newName[0].toUpperCase() + newName.slice(1);
     paragraf.textContent = newName2;
 
     img.src = typeLink.value;
 
-    comment.textContent = commentEnter.value;
+    //comment.textContent = newcomment1
+function checkSpam () {
+    let comment1 = commentEnter.value.replace('ViAgRA','***');
+    let newComment1 = comment1;
+    comment.textContent = newComment1;
 
-    }
+    //let comment2 = commentEnter.value.replace('xxx', '***');
+    //let newcomment2 = comment2;
+    //comment.textContent = newcomment2;
+}
+}
+
+
+//const comment1 = checkSpam('buy ViAgRA now'); //результат buy *** now
+//const comment2 = checkSpam('free xxx'); //результат free ***
+//const comment3 = checkSpam("innocent rabbit") ; //результат innocent rabbit
+    //let comment1 = commentEnter.value.replace(ViAgRA/i, '***');
+    //let comment2 = commentEnter.value.replace(xxxx/i, '***');
 
 
 let formElement = document.getElementById('form-comment');
