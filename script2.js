8d13481bf805befc4e2291449046c71800b2542e
your-m1nd <vi.neurogirl@gmail.com>
2023-05-26 08:50:24 +0300

    add script

--- script2.js
+++ script2.js
@@ -13,22 +13,7 @@ let commentEnter = document.getElementById('commentEnter');
 let comment = document.getElementById('comment');
 commentEnter.addEventListener('keyup', function(evt) {
 
-    function checkSpam() {
-      let comment1 = commentEnter.value.includes('ViAgRa');
-      console.log(comment1);
-    }
 })
-
-
-
-//function checkSpam(str) {
-
-    //let newStr = commentEnter.value;
-
- //'buy ViAgRA now'.includes('ViAgRa').replace(ViAgRA/i, '***'); // true
-//'free xxxxx'.includes('xxxxx').replace(xxx/i, '***');
-//}
-
 
 let send = document.getElementById('send');
     send.onclick = function getInputValue() {
@@ -39,24 +24,9 @@ let send = document.getElementById('send');
 
     img.src = typeLink.value;
 
-    //comment.textContent = newcomment1
-function checkSpam () {
-    let comment1 = commentEnter.value.replace('ViAgRA','***');
-    let newComment1 = comment1;
-    comment.textContent = newComment1;
+    comment.textContent = commentEnter.value;
 
-    //let comment2 = commentEnter.value.replace('xxx', '***');
-    //let newcomment2 = comment2;
-    //comment.textContent = newcomment2;
 }
-}
-
-
-//const comment1 = checkSpam('buy ViAgRA now'); //результат buy *** now
-//const comment2 = checkSpam('free xxx'); //результат free ***
-//const comment3 = checkSpam("innocent rabbit") ; //результат innocent rabbit
-    //let comment1 = commentEnter.value.replace(ViAgRA/i, '***');
-    //let comment2 = commentEnter.value.replace(xxxx/i, '***');
 
 
 let formElement = document.getElementById('form-comment');
